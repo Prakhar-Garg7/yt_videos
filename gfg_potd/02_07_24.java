c9865b1ad9036929110691a4c8c3d63276066dbc
Prakhar Garg <142152761+Prakhar-Garg7@users.noreply.github.com>
2024-07-02 12:13:18 +0530

    Update 02_07_24.java

--- gfg_potd/02_07_24.java
+++ gfg_potd/02_07_24.java
@@ -1,24 +1,19 @@
-import java.util.*;
-import java.io.*;
+// User function Template for Java
 class Solution {
-    public int[] intersect(int[] arr1, int[] arr2) {
-        int helper_size = 1001;
-        int[] helperArr1 = new int[1001];
-        int[] helperArr2 = new int[1001];
-        for(int x: arr1) helperArr1[x]++;
-        for(int x: arr2) helperArr2[x]++;
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < helper_size; i++){
-            int count = Math.min(helperArr1[i], helperArr2[i]);
-            for(int j = 0; j < count; j++) list.add(i);
+    public boolean compute(Node root) {
+        StringBuilder sb = new StringBuilder("");
+        Node ptr = root;
+        while(ptr != null) {
+            sb.append(ptr.data);
+            ptr = ptr.next;
         }
-        int sizeOfList = list.size();
-        int[] ans = new int[sizeOfList];
-        int ptr = 0;
-        for(int i = 0; i < sizeOfList; i++){
-            ans[ptr] = list.get(ptr);    
-            ptr++;
+        int len = sb.length();
+        int low = 0, high = len - 1;
+        while(low < high){
+            if(sb.charAt(low) != sb.charAt(high)) return false;
+            low++;
+            high--;
         }
-        return ans;
+        return true;
     }
 }
